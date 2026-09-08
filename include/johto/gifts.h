04ead414fc431cc9d77e201d33adaf52eae385b2
Esteban Mayorga <mayorgaa0722@gmail.com>
2026-09-08 21:23:32 +0200

    feat: add Johto gifts and shiny encounter adapters

---
+++ include/johto/gifts.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_JOHTO_GIFTS_H
+#define GUARD_JOHTO_GIFTS_H
+
+struct ScriptContext;
+
+void Script_JohtoGiveNamedMon(struct ScriptContext *ctx);
+void Script_JohtoGiveOddEgg(struct ScriptContext *ctx);
+void Script_JohtoMarkWildShiny(struct ScriptContext *ctx);
+
+#endif /* GUARD_JOHTO_GIFTS_H */
