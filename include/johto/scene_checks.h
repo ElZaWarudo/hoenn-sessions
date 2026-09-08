4865e9f7c905bafc3662f034cbcaf3a123779409
Esteban Mayorga <mayorgaa0722@gmail.com>
2026-09-08 21:23:29 +0200

    feat: add Johto scene eligibility checks

---
+++ include/johto/scene_checks.h
@@ -0,0 +1,13 @@
+#ifndef GUARD_JOHTO_SCENE_CHECKS_H
+#define GUARD_JOHTO_SCENE_CHECKS_H
+
+#include "gba/types.h"
+
+u16 Johto_CheckHooh(void);
+u16 Johto_CheckAerodactyl(void);
+u16 Johto_CheckKabuto(void);
+u16 Johto_CheckOmanyte(void);
+u16 Johto_CheckTogepi(void);
+u16 Johto_CheckCelebi(void);
+
+#endif // GUARD_JOHTO_SCENE_CHECKS_H
